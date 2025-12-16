b606308823805c9905e5a4f67c0f5c8bb6b62b70
TheOnlyMaro <marawanelsahy@gmail.com>
2025-12-16 23:18:59 +0200

    Create user schema

---
+++ reditto/server/models/User.js
@@ -0,0 +1,97 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  displayName: {
+    type: String,
+    trim: true,
+    maxlength: 30
+  },
+  avatar: {
+    type: String,
+    default: ''
+  },
+  karma: {
+    post: {
+      type: Number,
+      default: 0
+    },
+    comment: {
+      type: Number,
+      default: 0
+    }
+  },
+  communities: {
+    created: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Community'
+    }],
+    joined: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Community'
+    }]
+  },
+  savedPosts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  upvotedPosts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  downvotedPosts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  upvotedComments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  downvotedComments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  settings: {
+    lastActive: {
+      type: Date,
+      default: Date.now
+    }
+  }
+}, {
+  timestamps: true
+});
+
+// Index for faster queries
+userSchema.index({ username: 1 });
+userSchema.index({ email: 1 });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
