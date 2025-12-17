38c078afb39eba8eb6dd3fa6de73a34c922d6470
TheOnlyMaro <marawanelsahy@gmail.com>
2025-12-17 18:28:03 +0200

    Design alert and loading

--- reditto/src/components/index.js
+++ reditto/src/components/index.js
@@ -3,3 +3,5 @@ export { default as Header } from './Header/Header';
 export { default as Input } from './Input/Input';
 export { default as Card } from './Card/Card';
 export { default as Avatar } from './Avatar/Avatar';
+export { default as Loading } from './Loading/Loading';
+export { default as Alert } from './Alert/Alert';
